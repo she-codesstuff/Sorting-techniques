749ca7b31701b1882f5526c5b447c75480f2c9b1
Geetha K C <geethaaa373@gmail.com>
2026-01-27 20:17:40 +0530

    Add files via upload

---
+++ Basic_Sorting.java
@@ -0,0 +1,56 @@
+package sorting_algorithms;
+
+public class Basic_Sorting {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1,-1};
+        int n = arr.length;
+        print(arr);
+        for (int j = 0; j <= n; j++) {
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+            print(arr);
+
+    }
+//     for(int i = 0;i<n-1;i++) {
+//        if (arr[i] > arr[i+1]) {
+//            int temp = arr[i];
+//            arr[i] = arr[i+1];
+//            arr[i+1] = temp;
+//        }
+//    }
+//    print(arr);
+//
+//      for(int i = 0;i<n-1;i++) {
+//        if (arr[i] > arr[i+1]) {
+//       int temp = arr[i];
+//       arr[i] = arr[i+1];
+//        arr[i+1] = temp;
+//            }
+//                    }
+//print(arr);
+//
+//            for(int i = 0;i<n-1;i++) {
+//        if (arr[i] > arr[i+1]) {
+//          int temp = arr[i];
+//        arr[i] = arr[i+1];
+//          arr[i+1] = temp;
+//            }
+//                    }
+//            print(arr);
+//        }
+
+
+        static void print ( int[] arr){
+            for (int ele : arr) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+
